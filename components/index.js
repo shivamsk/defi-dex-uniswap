f175c8409bbd3c2812df6cff15a0123d3e2407a2
Siva Krishna Merugu <msk.iitr@gmail.com>
2024-08-02 22:33:25 +0530

    Initial Commit

---
+++ components/index.js
@@ -0,0 +1,39 @@
+import Header from './Header';
+import Footer from './Footer';
+import Card from './Card';
+import HeroSection from './HeroSection';
+import Selector from './Selector';
+import SingleCard from './SingleCard';
+import SwapComponent from './SwapComponent';
+import SwapField from './SwapField';
+import Table from './Table';
+import TokenBalance from './TokenBalance';
+import TransactionStatus from './TransactionStatus';
+
+//SVG Icons 
+import Facebook from './SVG/Facebook';
+import Insta from './SVG/Insta';
+import Logo from './SVG/Logo';
+import Menu from './SVG/Menu';
+import Twitter from './SVG/Twitter';
+
+export {
+    Header, 
+    Footer,
+    Card, 
+    HeroSection, 
+    Selector, 
+    SingleCard, 
+    SwapComponent,
+    SwapField,
+    Table,
+    TokenBalance,
+    TransactionStatus,
+
+    // SVG Icons 
+    Facebook,
+    Insta,
+    Logo, 
+    Menu,
+    Twitter,
+}
---
+++ deploy/contracts/Dex.sol
@@ -0,0 +1,125 @@
+// SPDX-License-Identifier : GPL3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CustomToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol){
+        _mint(msg.sender, 10000000 * 10 ** 18 ); 
+    }    
+}
+
+contract CustomDex {
+    // Custom tokens to be initialized 
+    string[] public tokens = ["Tether USD", "BNB", "USD Coin", "stETH", "TRON", "Matic Token", "SHIBA INU", "Uniswap"];
+
+    // map to maintain the token and it's instances 
+    mapping(string => ERC20) public tokenInstanceMap;
+
+    uint256 ethVaue = 100000000000000;
+
+    struct History {
+        uint256 historyId; 
+        string tokenA;
+        string tokenB; 
+        uint256 inputValue;
+        uint256 outputVaue; 
+        address userAddress;
+    }
+
+    uint256 public _historyIndex;
+    mapping(uint256 => History) private histories; 
+
+    constructor(){
+        for(int i = 0; i < tokens.length ; i++){
+            CustomToken token = new CustomToken(tokens[i], tokens[i]);
+            tokenInstanceMap[tokens[i]] = token; 
+        }
+    }
+
+    function getBalance(string memory tokenName, address _address) public view returns (uint256) {
+        return tokenInstanceMap[token].balanceOf[_address];
+    }
+
+    function getTotalSupply(string memory tokenName) public view returns (uint256) {
+        return tokenInstanceMap[tokenName].totaSupply();
+    }
+
+    function getName(string memory tokenName) public view returns (string memory){
+        return tokenInstanceMap[tokenName].name();
+    }
+
+    function getTokenAddress(string memory tokenName) public view returns (address){
+        return address(tokenInstanceMap[tokenName]);
+    }
+
+    function getEthBalance() public view returns (uint256){
+        return address(this).balance;
+    }
+
+    function _transactionHistory(string memory tokenName, string memory etherToken, uint256 inputVaue, uint256 outputVaue) internal {
+        _historyIndex++;
+        uint256 historyId = _historyIndex; 
+        History storage history = histories[historyId];
+
+        history.historyId = historyId;
+        history.userAddress = msg.sender;
+        history.tokenA = tokenName;
+        history.tokenB = etherToken; 
+        history.inputValue = inputValue; 
+        history.outputVaue = outputVaue;         
+    }
+
+    function swapEthToToken(string memory tokenName) public payable returns (uint256) {
+        uint256 inputVaue = msg.value;
+        uint256 outputValue = (inputVaue / ethVaue) * 10 ** 18;
+
+        require(tokenInstanceMap[tokenName].transfer(msg.sender, outputValue));
+
+        string memory etherToken = "Ether";
+
+        _transactionHistory(tokenName, etherToken, inputVaue, outputValue );
+
+        return outputValue;   
+    }
+
+    function swapTokenToEth(string memory tokenName, uint256 amount) public returns (uint256){
+        uint256 exactAmount = amount/10 ** 18;
+        uint256 ethToBeTransferred = exactAmount * ethValue; 
+
+        require( address(this).balance >= ethToBeTransferred , "Dex is running low on balance.");
+
+        payable(msg.sender).transfer(ethToBeTransferred);
+
+        require(tokenInstanceMap[tokenName].transferFrom(msg.sender, address(this), amount));
+
+        string memory etherToken = "Ether";
+
+        _transactionHistory(tokenName , etherToken, exactAmount , ethToBeTransferred);
+        return ethToBeTransferred; 
+    }
+
+    function swapTokenToToken(string memory srcTokenName, string memory destTokenName, uint256 amount) public {
+        require(tokenInstanceMap[srcTokenName].transferFrom(msg.sender, address(this) , amount));
+
+        require(tokenInstanceMap[destTokenName].transfer(msg.sender, amount));
+
+        _transactionHistory(srcTokenName, destTokenName, amount, amount); 
+    }
+
+    function getAllHistory() public view return (History[] memory){
+        uint256 itemCount= _historyIndex; 
+
+        uint256 currentIndex = 0; 
+
+        History[] memory items = new History[](itemCount);
+
+        for(int i = 1 ; i <= itemCount; i++){
+            items[i] = histories[i];
+        }
+
+        return items;
+    }
+
+}
